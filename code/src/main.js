b1b4bdd63f6f626541352ee1bb882198671d6fcf
hamauramakoto <makotarov2@gmail.com>
2021-01-20 09:02:10 +0900

    Vueの環境構築

--- code/src/main.js
+++ code/src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+// import './plugins/vuetify'
+import '/usr/src/app/src/plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  // vuetify,
+
+  // h関数はCreateElement関数のAlias
   render: h => h(App)
 }).$mount('#app')
--- code/src/router/index.js
+++ code/src/router/index.js
@@ -25,5 +25,6 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+console.log(process.env)
 
 export default router
